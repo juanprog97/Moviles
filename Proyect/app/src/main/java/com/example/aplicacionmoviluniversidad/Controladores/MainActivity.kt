a54ab2279e09cae1c7e8d39026785c4fe4e778c4
juanprog97 <juancard190@gmail.com>
2019-05-23 18:42:21 -0500

    "Agregando vista Principal"

---
+++ Proyect/app/src/main/java/com/example/aplicacionmoviluniversidad/Controladores/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.aplicacionmoviluniversidad.Controladores
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.example.aplicacionmoviluniversidad.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.menu_principal)
+    }
+}
